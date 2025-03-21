5bcafe5e89178361bfd369fbc849958ec72bbcaa
Siva Sai <sivasaikrishnassk@gmail.com>
2025-03-21 15:54:41 +0530

    Siva | add redis configuration

--- src/core/config.py
+++ src/core/config.py
@@ -10,17 +10,26 @@ class Config:
     ENVIRONMENT = os.getenv("ENVIRONMENT", "production")
     IS_TESTING = is_testing(ENVIRONMENT)
 
+    # Database configuration
     DB_HOST = os.getenv("DB_HOST")
     DB_PORT = os.getenv("DB_PORT")
     DB_USER = os.getenv("DB_USER")
     DB_PASSWORD = os.getenv("DB_PASSWORD")
     DB_NAME = os.getenv("DB_NAME")
    
+    # Test database configuration
     TEST_DB_HOST = os.getenv("TEST_DB_HOST")
     TEST_DB_PORT = os.getenv("TEST_DB_PORT")
     TEST_DB_USER = os.getenv("TEST_DB_USER")
     TEST_DB_PASSWORD = os.getenv("TEST_DB_PASSWORD")
     TEST_DB_NAME = os.getenv("TEST_DB_NAME")
+    
+    # Redis configuration
+    REDIS_HOST = os.getenv("REDIS_HOST")
+    REDIS_PORT = int(os.getenv("REDIS_PORT"))
+    REDIS_DB = int(os.getenv("REDIS_DB"))
+    
+    REDIS_URL = f"redis://{REDIS_HOST}:{REDIS_PORT}/{REDIS_DB}"
 
     SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
     ASYNC_SQLALCHEMY_DATABASE_URL = f"postgresql+asyncpg://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
